94b89132b766675df91cc52a898c1c79122700d1
BERNA ÖZGEN <brnozgen@hotmail.com>
2022-12-27 04:05:09 +0300

    update

---
+++ src/test/java/kodlamaio/com/rentACar/RentACarApplicationTests.java
@@ -0,0 +1,13 @@
+package kodlamaio.com.rentACar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
